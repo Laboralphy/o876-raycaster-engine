b34c84104a3505d9784d8321327a29b958e3e0c9
raphael marandet <raphael.marandet@gmail.com>
2019-02-11 16:47:15 +0100

    starting

---
+++ src/tag-grid/TagGrid.js
@@ -0,0 +1,15 @@
+// a grid of tags
+
+import Grid from "../grid";
+
+class TagGrid extends Grid {
+    /**
+     * adds a new tag on the specified cell
+     * @param x {number} cell coordinates (x axis)
+     * @param y {number} cell coordinates (y axis)
+     * @param sTag {string} tag
+     */
+    addTag(x, y, sTag) {
+        
+    }
+}
