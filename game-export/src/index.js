5acedbcf30e0eff225ecc10141d131603d83bfc2
raphael marandet <raphael.marandet@gmail.com>
2019-06-23 19:01:42 +0200

    filters added

--- game-export/src/index.js
+++ game-export/src/index.js
@@ -1,22 +1,34 @@
 import GameAbstract from '../../lib/src/GameAbstract';
 import FadeOut from '../../lib/src/engine/filters/FadeOut';
-import Gradient from '../../lib/src/engine/filters/Gradient';
-import DarkHalo from '../../lib/src/engine/filters/DarkHalo'
+import Halo from '../../lib/src/engine/filters/Halo';
+import FadeIn from "../../lib/src/engine/filters/FadeIn";
+import Timed from "../../lib/src/engine/filters/Timed";
 
 
 class Game extends GameAbstract {
 
 }
 
-function main() {
+async function main() {
     const g = new Game();
-    g.run();
+    await g.run();
+    g.engine.filters.link(new Halo('black'));
+    const fi = new FadeIn({duration: 500});
+    g.engine.filters.link(fi);
 
     window.addEventListener('keypress', event => {
         if (event.key === 'f') {
-            g.engine.filters.link(new DarkHalo());
+            g.engine.filters.link(new FadeIn({color: 'pink', duration: 2000}));
+        }
+        if (event.key === 'g') {
+            g.engine.filters.link(new FadeOut({duration: 500}));
+        }
+        if (event.key === 'h') {
+            g.engine.filters.link(new Timed({duration: 2000, child: new FadeOut({duration: 500})}));
         }
     });
+
+
 }
 
 window.addEventListener('load', main);
