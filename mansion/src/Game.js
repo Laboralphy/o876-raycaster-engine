e6b2b4c41e50244cf3345ffe8bf6ffa5c70b0d40
Laboralphy <raphael.marandet@gmail.com>
2019-08-11 10:28:35 +0200

    going to add door events

--- mansion/src/Game.js
+++ mansion/src/Game.js
@@ -2,6 +2,13 @@ import GameAbstract from '../../lib/src/game-abstract';
 
 class Game extends GameAbstract {
     // ... write your game here ...
+
+    init() {
+        super.init();
+
+        // listen to events
+
+    }
 }
 
 export default Game;
