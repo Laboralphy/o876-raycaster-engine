2ba958b392bd4fe57390bf4d4812a7ebb3889fa3
raphael marandet <raphael.marandet@gmail.com>
2019-08-13 15:00:46 +0200

    importing fix

--- mansion/src/Game.js
+++ mansion/src/Game.js
@@ -7,7 +7,21 @@ class Game extends GameAbstract {
         super.init();
 
         // listen to events
+    }
 
+    /**
+     * Returns a list of all tags present on the maps
+     * the list contains items with these properties :
+     * {
+     *     x, y, // cell coordinates
+     *     tags,  // tags
+     * }
+     * @return {array}
+     */
+    getLevelTags() {
+        this.engine._tm._tg.iterate((x, y, cell) => {
+            console.log(x, y, cell);
+        })
     }
 }
 
